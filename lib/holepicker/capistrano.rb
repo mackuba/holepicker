82677d529347a02490f1f385f35fc64ca1c3a589
Manuel van Rijn <manuel@manuelles.nl>
2013-02-25 10:19:19 +0100

    fixed before_deploy and execute scanner instead of command

--- lib/holepicker/capistrano.rb
+++ lib/holepicker/capistrano.rb
@@ -1,14 +1,24 @@
+require 'holepicker/scanner'
+
 Capistrano::Configuration.instance.load do
-  before "deploy", "holepicker"
+  set(:holepicker_offline, false) unless exists?(:holepicker_offline)
+  set(:holepicker_dont_skip, false) unless exists?(:holepicker_dont_skip)
+  set(:holepicker_ignored_gems, []) unless exists?(:holepicker_ignored_gems)
+
+  before "deploy:update_code", "holepicker"
 
   namespace :holepicker do
     desc "Look for vulnerabilities in your Gemfile"
     task :default, :roles => :app do
+      options = {
+        :ignored_gems => holepicker_ignored_gems,
+        :offline => holepicker_offline,
+        :dont_skip => holepicker_dont_skip
+      }
+
       gemfile_lock = "#{ENV['BUNDLE_GEMFILE']}.lock"
-      # using %x[] untill capistrano's run_locally returns output
-      output = %x[bundle exec holepicker #{gemfile_lock}]
-      unless $? == 0
-        logger.important output
+      success = HolePicker::Scanner.new(gemfile_lock, options).scan
+      unless success
         raise Capistrano::CommandError.new("HOLEPICKER found vulnerabilities!")
       end
     end
